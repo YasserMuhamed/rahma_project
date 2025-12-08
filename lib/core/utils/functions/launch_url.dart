c0adcd41de6c10fda8b360a16d6423187b6d9ba5
Yasser Mohamed <yasser.muhamed0001@gmail.com>
2025-12-08 19:56:04 +0200

    - remove unused dependencies

--- lib/core/utils/functions/launch_url.dart
+++ lib/core/utils/functions/launch_url.dart
@@ -1,22 +1,22 @@
-import 'package:logger/logger.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:logger/logger.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-Future<void> launchUrlHelper({
-  required String url,
-  bool? isPhone,
-  bool? isEmail,
-}) async {
-  if (isPhone == true) {
-    url = 'tel:$url';
-  }
-  if (isEmail == true) {
-    url = 'mailto:$url';
-  }
-  final Uri uri = Uri.parse(url);
-  if (await canLaunchUrl(uri)) {
-    await launchUrl(uri, mode: LaunchMode.externalApplication);
-    Logger().d('Launched $url');
-  } else {
-    Logger().e('⛔ Could not launch ${uri.toString()}');
-  }
-}
+// Future<void> launchUrlHelper({
+//   required String url,
+//   bool? isPhone,
+//   bool? isEmail,
+// }) async {
+//   if (isPhone == true) {
+//     url = 'tel:$url';
+//   }
+//   if (isEmail == true) {
+//     url = 'mailto:$url';
+//   }
+//   final Uri uri = Uri.parse(url);
+//   if (await canLaunchUrl(uri)) {
+//     await launchUrl(uri, mode: LaunchMode.externalApplication);
+//     Logger().d('Launched $url');
+//   } else {
+//     Logger().e('⛔ Could not launch ${uri.toString()}');
+//   }
+// }
